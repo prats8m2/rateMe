27b38473a5fdc1d9517cf7269a61fc7a7d3e4d4a
prats8m2 <prateek3693@gmail.com>
2020-01-09 18:57:14 +0530

    updated db config

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = '';
-const MONGO_PASSWORD = '';
-const MONGO_HOSTNAME = '127.0.0.1';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'test_rate_me';
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-
-mongoose.connect(url, {useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/rateMe',
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => {
+        console.log('Connected to database!');
+    }).catch(error => {
+        console.log('Connection failed!');
+        console.log(error);
+    }); 
